cd0cff21a247e56978c520c1b6b5f5c961553d5e
SzDave <szantodavid.mail@gmail.com>
2025-01-13 22:50:05 +0100

    Add TODOs to the App class.

--- src/main/java/com/_2p1team/cmadmin/app/App.java
+++ src/main/java/com/_2p1team/cmadmin/app/App.java
@@ -4,6 +4,11 @@ import com._2p1team.cmadmin.app.view.frame.AppFrame;
 
 public final class App {
     public static void main(String[] args) {
+        /*
+         * TODO: Implement language
+         *       Work out a better way for the Settings to be loaded and handled
+         *       Work out a better way for size calculations
+         * */
         AppFrame frame = new AppFrame(null, "Test Frame");
     }
 }
