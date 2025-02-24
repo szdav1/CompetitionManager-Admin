747c288238cb967b2556230f3a1113508011e3e3
szdav1 <szantodavid.mail@gmail.com>
2025-02-25 00:51:05 +0100

    Note future jobs.

--- src/main/java/com/_2p1team/cmadmin/app/AdminApp.java
+++ src/main/java/com/_2p1team/cmadmin/app/AdminApp.java
@@ -5,6 +5,8 @@ import com._2p1team.cmadmin.support.util.Util;
 
 public final class AdminApp {
 
+    // TODO: Start making the table
+    // TODO: After completing the table, merge the poule and the table together to create the competition
     public static void main(String[] args) {
         AppFrame frame = new AppFrame(Util.loadImageIcon("/assets/appIcon.png").getImage(), "CompetitionManager - Admin");
     }
