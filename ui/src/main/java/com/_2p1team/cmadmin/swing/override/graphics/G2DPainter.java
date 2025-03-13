c37f92b8a6b1dadf4f05beba96824771e26958cf
szdav1 <szantodavid.mail@gmail.com>
2025-03-13 09:36:01 +0100

    Finish javadoc for the UI module.

--- ui/src/main/java/com/_2p1team/cmadmin/swing/override/graphics/G2DPainter.java
+++ ui/src/main/java/com/_2p1team/cmadmin/swing/override/graphics/G2DPainter.java
@@ -11,10 +11,34 @@ import javax.swing.JComponent;
 import java.awt.*;
 import java.awt.geom.Point2D;
 
-// TODO: Write javadoc
+/**
+ * The G2DPainter class is the one that is responsible for
+ * the custom painting methods of components inside the package.
+ * It should be called from the custom defined component's overridden
+ * paint method, with casting the default Graphics object passed as a parameter
+ * to a Graphics2D object, which will be used to perform the different painting methods,
+ * hence the name G2DPainter.
+ *
+ * @see Graphics2D
+ * @see Appearance
+ * @see AppearanceComponent
+ */
 @AllArgsConstructor(access=AccessLevel.PRIVATE)
 public final class G2DPainter {
 
+    /**
+     * Determines the paint of the background based on the passed in Appearance's current state.
+     * Handles colors and fractions in case of a linear gradient background paint, and single colors
+     * in case of a simple paint. The returned Paint object will be determined based on the passed in
+     * start, end points and the Appearance. If the component is not enabled, the default paint will be returned.
+     *
+     * @param start      The starting point of the paint
+     * @param end        The end point of the paint
+     * @param appearance The component's Appearance object from which configuration data is fetched
+     * @param component  The component the created paint will be applied to
+     * @param <C>        Type that is a child of JComponent and implements the AppearanceComponent interface
+     * @return The Paint object created based on the passed in parameters
+     */
     private static <C extends JComponent & AppearanceComponent> LinearGradientPaint determineBackgroundPaint(final Point2D start, final Point2D end, final Appearance appearance, final C component) {
         if (appearance.getState() == UIState.ERROR)
             return new LinearGradientPaint(
@@ -81,6 +105,16 @@ public final class G2DPainter {
             );
     }
 
+    /**
+     * Paints the background of the specified component with the determined paint.
+     *
+     * @param g2         The Graphics2D object from the component's graphics configuration
+     * @param start      The starting point of the paint
+     * @param end        The end point of the paint
+     * @param appearance The component's Appearance object from which configuration data is fetched
+     * @param component  The component the created paint will be applied to
+     * @param <C>        Type that is a child of JComponent and implements the AppearanceComponent interface
+     */
     private static <C extends JComponent & AppearanceComponent> void paintBackground(final Graphics2D g2, Point2D start, Point2D end, final Appearance appearance, final C component) {
         g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         LinearGradientPaint lgp = determineBackgroundPaint(start, end, appearance, component);
@@ -96,6 +130,19 @@ public final class G2DPainter {
         );
     }
 
+    /**
+     * Determines the paint of the border based on the passed in Appearance's current state.
+     * Handles colors and fractions in case of a linear gradient border paint, and single colors
+     * in case of a simple paint. The returned Paint object will be determined based on the passed in
+     * start, end points and the Appearance. If the component is not enabled, the default paint will be returned.
+     *
+     * @param start      The starting point of the paint
+     * @param end        The end point of the paint
+     * @param appearance The component's Appearance object from which configuration data is fetched
+     * @param component  The component the created paint will be applied to
+     * @param <C>        Type that is a child of JComponent and implements the AppearanceComponent interface
+     * @return The Paint object created based on the passed in parameters
+     */
     private static <C extends JComponent & AppearanceComponent> LinearGradientPaint determineBorderPaint(final Point2D start, final Point2D end, final Appearance appearance, final C component) {
         return appearance.isInteractivityEnabled() && component.isEnabled() ?
             new LinearGradientPaint(
@@ -148,6 +195,16 @@ public final class G2DPainter {
             );
     }
 
+    /**
+     * Paints the border of the specified component with the determined paint.
+     *
+     * @param g2         The Graphics2D object from the component's graphics configuration
+     * @param start      The starting point of the paint
+     * @param end        The end point of the paint
+     * @param appearance The component's Appearance object from which configuration data is fetched
+     * @param component  The component the created paint will be applied to
+     * @param <C>        Type that is a child of JComponent and implements the AppearanceComponent interface
+     */
     private static <C extends JComponent & AppearanceComponent> void paintBorder(final Graphics2D g2, Point2D start, Point2D end, final Appearance appearance, final C component) {
         if (appearance.getBorderConfiguration().getThickness() <= 0 && appearance.getBorderConfiguration().getRadius() <= 0)
             return;
@@ -167,6 +224,14 @@ public final class G2DPainter {
         );
     }
 
+    /**
+     * Sets the foreground of the specified component based on its Appearance's current state.
+     * Note that this method will not have any effects on non-text-displaying components.
+     *
+     * @param g2        The Graphics2D object from the component's graphics configuration
+     * @param component The component the created paint will be applied to.
+     * @param <C>       Type that is a child of JComponent and implements the AppearanceComponent interface
+     */
     public static <C extends JComponent & AppearanceComponent> void paint(final Graphics2D g2, final C component) {
         Point2D start = new Point2D.Double(0, 0);
         Point2D end = new Point2D.Double(component.getWidth(), component.getHeight());
@@ -197,6 +262,13 @@ public final class G2DPainter {
         );
     }
 
+    /**
+     * Sets the foreground of the specified component based on its Appearance's current state.
+     * Note that this method will not have any effects on non-text-displaying components.
+     *
+     * @param component The Component that's foreground needs to be set
+     * @param <C>       Type that is a child of JComponent and implements the AppearanceComponent interface
+     */
     private static <C extends JComponent & AppearanceComponent> void setForegroundOfComponent(final C component) {
         component.setForeground(
             switch (component.getComponentAppearance().getState()) {
@@ -219,6 +291,14 @@ public final class G2DPainter {
         );
     }
 
+    /**
+     * Sets the icon of the specified component based its Appearance's current state.
+     * Note that this method will only be performed if the specified Component is an
+     * instance of the JLabel or JButton class.
+     *
+     * @param component The Component that's icon needs to be set
+     * @param <C>       Type that is a child of JComponent and implements the AppearanceComponent interface
+     */
     private static <C extends JComponent & AppearanceComponent> void setIconOfComponent(final C component) {
         if (component instanceof AbstractLabel label) {
             label.setIcon(
@@ -264,6 +344,13 @@ public final class G2DPainter {
         }
     }
 
+    /**
+     * Draws the specified Component's UI elements, that includes setting the font, foreground and icon
+     * of the component. Note that this method will not have any effects on non-text-displaying components.
+     *
+     * @param component The component that's UI elements should be painted
+     * @param <C>       Type that is a child of JComponent and implements the AppearanceComponent interface
+     */
     public static <C extends JComponent & AppearanceComponent> void drawComponentUIElements(final C component) {
         if (!component.getComponentAppearance().isInteractivityEnabled() || !component.isEnabled())
             return;
@@ -273,12 +360,26 @@ public final class G2DPainter {
         setIconOfComponent(component);
     }
 
+    /**
+     * Paints the track of a scroll bar based on the passed in Appearance's configurations.
+     *
+     * @param g2          The Graphics2D object from the component's graphics configuration
+     * @param paintBounds The bounds the paint should be within
+     * @param appearance  The component's Appearance object from which configuration data is fetched
+     */
     public static void paintScrollBarTrack(final Graphics2D g2, final Rectangle paintBounds, final Appearance appearance) {
         g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2.setColor(appearance.getForegroundConfiguration().getDefaultColor());
         g2.fillRoundRect(paintBounds.x, paintBounds.y, paintBounds.width, paintBounds.height, appearance.getBorderConfiguration().getRadius(), appearance.getBorderConfiguration().getRadius());
     }
 
+    /**
+     * Creates the paint of the thumb of the scroll bar.
+     *
+     * @param paintBounds The bounds the paint should be within
+     * @param appearance  The component's Appearance object from which configuration data is fetched
+     * @return The created Paint object
+     */
     private static LinearGradientPaint createScrollBarThumbPaint(final Rectangle paintBounds, final Appearance appearance) {
         return new LinearGradientPaint(
             paintBounds.x,
@@ -294,6 +395,13 @@ public final class G2DPainter {
         );
     }
 
+    /**
+     * Paints the thumb of the scroll bar based on the Appearance's configurations.
+     *
+     * @param g2          The Graphics2D object from the component's graphics configuration
+     * @param paintBounds The bounds the paint should be within
+     * @param appearance  The component's Appearance object from which configuration data is fetched
+     */
     public static void paintScrollBarThumb(final Graphics2D g2, final Rectangle paintBounds, final Appearance appearance) {
         g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2.setPaint(createScrollBarThumbPaint(paintBounds, appearance));
